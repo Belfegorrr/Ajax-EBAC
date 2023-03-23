e0cf6dfa73eb1e28a846eb28d2d6d0d6c2e3af57
Belfegorrr <pelichek.iago@gmail.com>
2023-03-23 02:21:11 -0300

    adicionando requisição ajax para cep

---
+++ main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("btn-buscar-cep")
+    .addEventListener("click", function () {
+      const xhttp = new XMLHttpRequest();
+      const cep = document.getElementById("cep").value;
+      const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+
+      xhttp.open("GET", endpoint);
+      xhttp.send();
+
+      // https://viacep.com.br/ws/32165/json
+    });
+});
