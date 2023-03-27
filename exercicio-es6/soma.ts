bf117aa952ee4ef37cc3eecd9eac994169c1cf21
Belfegorrr <pelichek.iago@gmail.com>
2023-03-27 04:12:11 -0300

    função com Typescript

---
+++ exercicio-es6/soma.ts
@@ -0,0 +1,10 @@
+function multiplicaDoisNumeros( a: number, b: number): number {
+    return a * b;
+}
+
+function concatString(nome: string): string {
+    return "Olá" + nome
+}
+
+console.log(multiplicaDoisNumeros(4,8))
+console.log(concatString("Iago"))
